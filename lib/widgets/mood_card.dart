dd247669efbff287f330b265be9f1f83df91bf9a
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2024-11-13 14:47:02 +0700

    deleted the todos (maaf lupa delete tadi kak)

--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -49,7 +49,6 @@ class ItemCard extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => const MoodEntryPage()));
           } else if (item.name == "Logout") {
             final response = await request.logout(
-                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                 "http://127.0.0.1:8000/auth/logout/");
             String message = response["message"];
             if (context.mounted) {
